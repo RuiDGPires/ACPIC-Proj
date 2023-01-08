86821743e569b14b6d42f032064c45b489d95739
RuiDGPires <RuiDGPires@users.noreply.github.com>
2023-01-08 12:24:07 +0000

    Messages stuff

--- Trafic Lights/main.cpp
+++ Trafic Lights/main.cpp
@@ -4,22 +4,22 @@
 #include "TLKB/main.hpp"
 #include "main.hpp"
 
-#ifndef __MAIN__
-    void setup() {
-        tl_setup();
-    }
+// ------
+//  UTIL
+// ------
 
-    void loop() {
-        tl_loop();
-    }
-#endif
+#define N2(n) n, n+1
+#define N3(n) N2(n), n+2
+#define N4(n) N3(n), n+3
+#define N5(n) N4(n), n+4
+#define N6(n) N5(n), n+5
+#define N7(n) N6(n), n+6
+#define N8(n) N7(n), n+7
+#define N9(n) N8(n), n+8
+#define N10(n) N9(n), n+9
+#define N11(n) N10(n), n+10
 
-void tl_setup() {
-    int entryNumber = getEntryNumber();
-    Wire.begin(entryNumber);
-    Wire.onReceive(receiveEvent);
-    Wire.onRequest(requestEvent);
-
+void tl_setup(int tlka_g, int, int, int, int, int) {
     tlka_setup(1, 2, 3);
     tlkb_setup();
 }
@@ -55,20 +55,59 @@ struct message_t {
     Operation op;
 }
 
+char checksum(const char buffer[], int integrity_index) {
+    // Checksum
+    char sum = 0;
+    for (int i = 0; i < integrity_index; i++)
+        sum += buffer[i];
+
+    return sum;
+}
+
 void parse_message(const char buffer[], struct message_t *msg) {
     // UNSAFE | NO CHECKS FOR THE BUFFER LENGTH
     msg->sender         = (unsigned int) buffer[0];
     msg->op             = (Operation) buffer[1];
     msg->destination    = (unsigned int) buffer[2];
-
+    
     if (msg->op == OP_STATUS)
         msg->info = (unsigned int) buffer[3];
+
+    const int integrity = msg->op == OP_STATUS ? 4 : 3;
+    const char sum = checksum(buffer, integrity);
+    if (sum != buffer[integrity])
+        return; // TODO : ?
+}
+
+// If this is the main file for the arduino
+#ifndef __MAIN__ 
+
+const int entryNumber = getEntryNumber();
+
+void setup() {
+    Wire.begin(entryNumber);
+    Wire.onReceive(receiveEvent);
+    Wire.onRequest(requestEvent);
+
+    tl_setup(N6(1));
+}
+
+void loop() {
+    tl_loop();
 }
 
 void receiveEvent() {
+    char buffer[20];
 
+    // Read incoming string
+    for (int i = 0; Wire.available(); i++)
+        buffer[i] = Wire.read();
+
+    
 }
 
 void requestEvent() {
 
 }
+
+#endif
